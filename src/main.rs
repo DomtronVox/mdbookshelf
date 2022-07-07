ee9d99a61dfdc15f865d706e0d7ff2b72208a0fe
Domtron Vox <domtron.vox@gmail.com>
2022-07-06 23:13:11 -0400

    Greatly simplifyed the path juggling by switching to using absolute paths for everything instead of trying to mangle relative paths differently for the different types.

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,11 @@ const DEFAULT_SOURCE_DIR: &str = "bookshelf";
 const DEFAULT_BUILD_DIR: &str = "build";
 
 
-
-///Enume indicating the type of book and path to it's source info
+///Enum indicating the type of book.
+#[derive(PartialEq)]
 enum BookType {
-    MDBook(PathBuf),
-    PDF(PathBuf),
+    MDBook,
+    PDF,
 }
 
 
@@ -41,26 +41,21 @@ fn is_book(entry: &DirEntry) -> Option<BookType> {
     // mdbook config file detection    
     if file_name == "book.toml" {
         //note that mdbook wants the directory not the actual book.toml file so we use parent()
-        Option::Some( BookType::MDBook(
-            entry.path()
-                 .parent() 
-                 .expect("Failed to aquire parent directory of book.toml. This should not have happened!")
-                 .to_path_buf()
-        ))
+        Option::Some( BookType::MDBook )
     
     // PDF file detection
     } else if file_name.ends_with(".pdf") {
-        Option::Some( BookType::PDF( entry.path().to_path_buf() ) )
+        Option::Some( BookType::PDF )
 
     //catch all for all other files and directories
     } else {
         Option::None
     }
 }
-
+ 
 
 ///Walks a given path looking for either PDFs or book.tomel files
-fn find_books(path: &str) -> Vec<BookType> {
+fn find_books(path: &PathBuf) -> Vec<(BookType, PathBuf)> {
     let mut book_list = vec!();
 
     //build the iterator so we can start the dir walk
@@ -68,107 +63,129 @@ fn find_books(path: &str) -> Vec<BookType> {
     
     //we need to control the iterator a little more closely then a simple for loop allows
     loop {
-        //aquire next entry
+        
+        //acquire next entry
         let entry = match it.next() {
             Some(Ok(entry)) => entry,
             None => break, //if there is no next we end the loop
             _ => continue, //we just skip error files and 
         };
         
-        //test if entry denotes a valid book
-        let book_data = match is_book(&entry) {
+        
+        //test if entry denotes a valid book we can recognize
+        let book_type = match is_book(&entry) {
             //if not we continue loop to the next iteration
             None => continue,            
             //if so we return it to be processed
             Some(book_type) => book_type
         };
         
-        //we skip the whole directory if this is an mdbook as it uses whole directories
-        if let BookType::MDBook(_) = book_data  {
+        
+        //we tell the iterator to skip the whole directory if this is an mdbook as it uses whole directories
+        if book_type == BookType::MDBook {
             it.skip_current_dir();
         }
         
-        book_list.push(book_data);
+
+        //figure out source path 
+        let source_path = match book_type {
+            BookType::MDBook => 
+                entry.path()
+                     .parent() 
+                     .expect("Failed to acquire parent directory of book. This should not have happened!")
+                     .to_path_buf(),
+        
+            BookType::PDF =>
+                entry.path().to_path_buf(),
+        };
+        
+        book_list.push( (book_type, source_path) );
     }
     
     book_list
 }
 
-///Inserts the given string as the first directory into the given path. 
-fn move_root_directory(path: &PathBuf, new_root: &str) -> PathBuf {
-    //we need to strip out the first folder level
-    let mut comps = path.components();
-    comps.next(); // skip ./
+///strips out everything from path before the source folder. 
+///Note that the partial path is a string and should be joined into a path/pathbuf when used
+fn isolate_partial_path(full_path: &PathBuf, source_path: &PathBuf) -> Result<PathBuf, String> {
+    let mut partial_path = PathBuf::from("");
     
-    //now we add in cwd then the new_root and finally the rest of the path
-    let mut new_path = PathBuf::from(".");
-    new_path.push(new_root);
-    new_path.push(comps.as_path());
-     
-    new_path
-}
-
-///because of the way mdbook works, this function reworks the source path so it points to a **relative**
-//  path from the mdbook source folder to the mdbookshelf build directory. it's a bit convoluted.
-fn move_mdbook_path_to_build(mdbook_source: &PathBuf, build_root: &str) -> PathBuf {
-    //first we create a relative path to the same location as the source data but under the build directory
-    let cwd_relative = move_root_directory(mdbook_source, build_root);
+    let mut source_iter = source_path.components();
     
-    //Because running build on mdbook struct works from the source data's folder NOT CWD, 
-    //  we have to adjust the relative path up to cwd then back down
-    let mut new_path = PathBuf::from(".");
-    
-    //essentially replace each directory component with "go up a level"
-    for comp in mdbook_source.components() {
-        if let Component::RootDir = comp { continue; } 
-        if let Component::CurDir = comp { continue; } 
-        new_path.push("..");
-    }
-    
-    //mangle everything together and return
-    new_path.join({ 
-        let mut comps = cwd_relative.components();
-        comps.next(); //strip out root
-        comps.as_path()
-    })
-}
-
-
-fn main() {
-
-    let book_list = find_books("./bookshelf");
-    
-    for book in book_list {
-        if let BookType::MDBook(path) = book {
-            let build_path = move_mdbook_path_to_build(&path, "build");
-            
-            println!("MDBook at {}, into (relative from previous path) {}", path.display(), build_path.display());
-            
-            //create book object from path which has the book.tomel and all needed info
-            let mut md = MDBook::load(&path)
-                .expect("Unable to load the book");
-                
-            //we need to set the output to be inside the bookshelf output 
-            md.config.build.build_dir = build_path;
-
-            //Try to build the book
-            md.build().expect("Building failed");
-            
-        } else if let BookType::PDF(path) = book {
-            let build_path = move_root_directory(&path, "build");
-            
-            println!("PDF at {}, into {}", path.display(), build_path.display());
-            
-            let mut directory_only = build_path.clone();
-            directory_only.pop();
-            println!("{}", directory_only.display());
-            println!("{:?}", fs::create_dir_all(directory_only));
-            println!("{:?}", fs::copy(&path, &build_path));
+    //we need to strip out everything up to the given source_dir
+    for component in full_path.components() {
+        let src_comp = source_iter.next();
+        
+        //strip out everything not matching the source path.
+        if src_comp != None {
+            if component != src_comp.unwrap() {
+                return Err(format!("Full path {:#?} does not match source path {:#?}. This should not be possible and is a bug. Terminating.", full_path, source_path).to_string());
+            }
+        
+        //now that the source path is removed, everything else is added to our new path.
+        } else if src_comp == None {
+            partial_path.push(component);
         }
     }
     
-    spawn_server("./build".to_string(), "127.0.0.1", "3000");
-    loop{}
+    Ok(partial_path)
+}
+
+
+
+fn main() {
+    //figure out the root source and build paths
+    let source_path = std::env::current_dir().unwrap().join("bookshelf");
+    let build_path = std::env::current_dir().unwrap().join("build");
+    
+    //compile list of books
+    let book_list = find_books(&source_path);
+    
+    //process books by either copying files or triggering MDBook builds
+    for book in book_list {
+        //just to be clear
+        let book_type = book.0;
+        let book_source_path = book.1;
+        
+        //get the part of the path unique to the source directory. We use this same relative
+        // path when placing stuff in the build dir.
+        let path_part = isolate_partial_path(&book_source_path, &source_path).unwrap();
+
+        //location to place the book.
+        let book_build_path = build_path.join("bookshelf").join(path_part);
+        
+        match book_type {
+        
+            BookType::MDBook => {
+            
+                println!("MDBook source {}, building into {}", book_source_path.display(), book_build_path.display());
+            
+                //create book object from path which has the book.tomel and all needed info
+                let mut md = MDBook::load(&book_source_path)
+                    .expect("Unable to load the book");
+                
+                //we need to set the output to be inside the buildpath /bookshelf directory 
+                md.config.build.build_dir = book_build_path;
+
+                //Try to build the book
+                md.build().expect("Building failed");
+            
+            },
+        
+            BookType::PDF => {
+                println!("PDF source {}, copying into {}", book_source_path.display(), book_build_path.display());
+            
+                let mut directory_only = book_build_path.clone();
+                directory_only.pop();
+                println!("{}", directory_only.display());
+                println!("{:?}", fs::create_dir_all(directory_only));
+                println!("{:?}", fs::copy(&book_source_path, &book_build_path));
+            },
+        }
+    }
+    
+    //spawn_server("./build".to_string(), "127.0.0.1", "3000");
+    //loop{}
 }
 
 
